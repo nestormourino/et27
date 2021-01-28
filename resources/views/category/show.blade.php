96f7d2c8d62ab4cd4e593d36ff591a2157e79ccb
archeusWLA <64558342+archeusWLA@users.noreply.github.com>
2021-01-28 04:41:13 -0300

    Correción popup folleto GCBA

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -10,7 +10,7 @@
                             <div class="text-xl mb-4 text-gray-800 text-justify" style="padding-right:10px; padding-left:10px">
                                 <h1 class="py-2 mx-auto text-2xl leading-8 font-bold" style="border-bottom: 4px solid #1e4a6d; text-align:center">MATERIAS</h1>
                                 <img src="{{$category->link_image}}" alt="" class="mt-6">
-                                <span class="text-sm">*Según Resolución Nº 4149/SSGECP/2012 - <a href="https://www.buenosaires.gob.ar/sites/gcaba/files/folleto-tecnica-2013_web_0_0.pdf" class="font-bold">Acceda al folleto aquí</a> </span>
+                                <span class="text-sm">*Según Resolución Nº 4149/SSGECP/2012 - <a href="https://www.buenosaires.gob.ar/sites/gcaba/files/folleto-tecnica-2013_web_0_0.pdf" target="_blank" class="font-bold">Acceda al folleto aquí</a> </span>
                             </div>
                         </div>
                     </div>
