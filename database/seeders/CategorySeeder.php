06b43294532d9c2d2ea65baf2e53886845db44d3
archeusWLA <64558342+archeusWLA@users.noreply.github.com>
2021-01-28 04:22:54 -0300

    Actualizacion sistema de categorias

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -18,50 +18,50 @@ class CategorySeeder extends Seeder
             'name' => '1er año',
             'slug' => '1er-ano',
             'link_drive' => 'https://drive.google.com/drive/folders/1ofe-RfjyH1pCyt-bbbB1GnCalJBYG9_S?usp=sharing',
-            'link_image' => '',
-            'description' => ''
+            'link_image' => 'https://www.et27de18.com/public/images/primero.png',
+            'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores velit rerum sapiente vitae magnam fuga alias dolore quod odit suscipit odio, consequatur corrupti praesentium quas nesciunt reiciendis iusto. Velit, nisi?'
         ]);
         Category::create([
             'name' => '2do año',
             'slug' => '2do-ano',
             'link_drive' => 'https://drive.google.com/drive/folders/1hIohKvS7auAwS5ScgSBAALGtaZMdzG7L?usp=sharing',
-            'link_image' => '',
-            'description' => ''
+            'link_image' => 'https://www.et27de18.com/public/images/segundo.png',
+            'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores velit rerum sapiente vitae magnam fuga alias dolore quod odit suscipit odio, consequatur corrupti praesentium quas nesciunt reiciendis iusto. Velit, nisi?'
         ]);
         Category::create([
             'name' => '3er año',
             'slug' => '3er-ano',
             'link_drive' => 'https://drive.google.com/drive/folders/1z7oa6LHln5T6M91pNIccq4qlDOP45cTr?usp=sharing',
-            'link_image' => '',
-            'description' => ''
+            'link_image' => 'https://www.et27de18.com/public/images/tercero.png',
+            'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores velit rerum sapiente vitae magnam fuga alias dolore quod odit suscipit odio, consequatur corrupti praesentium quas nesciunt reiciendis iusto. Velit, nisi?'
         ]);
         Category::create([
             'name' => '4to año',
             'slug' => '4to-ano',
             'link_drive' => 'https://drive.google.com/drive/folders/1Fxv1fo6lK11dyB7rsd2ms98KiBKrqVvG?usp=sharing',
-            'link_image' => '',
-            'description' => ''
+            'link_image' => 'https://www.et27de18.com/public/images/cuarto.png',
+            'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores velit rerum sapiente vitae magnam fuga alias dolore quod odit suscipit odio, consequatur corrupti praesentium quas nesciunt reiciendis iusto. Velit, nisi?'
         ]);
         Category::create([
             'name' => '5to año',
             'slug' => '5to-ano',
             'link_drive' => 'https://drive.google.com/drive/folders/1FcgRYfNSq4FIHwmysPgVCpzhRcStxzap?usp=sharing',
-            'link_image' => '',
-            'description' => ''
+            'link_image' => 'https://www.et27de18.com/public/images/quinto.png',
+            'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores velit rerum sapiente vitae magnam fuga alias dolore quod odit suscipit odio, consequatur corrupti praesentium quas nesciunt reiciendis iusto. Velit, nisi?'
         ]);
         Category::create([
             'name' => '6to año',
             'slug' => '6to-ano',
             'link_drive' => 'https://drive.google.com/drive/folders/1fd41UaKHp1T-oNDYu7jOFoaMT6TPNgXj?usp=sharing',
-            'link_image' => '',
-            'description' => ''
+            'link_image' => 'https://www.et27de18.com/public/images/sexto.png',
+            'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores velit rerum sapiente vitae magnam fuga alias dolore quod odit suscipit odio, consequatur corrupti praesentium quas nesciunt reiciendis iusto. Velit, nisi?'
         ]);
         Category::create([
             'name' => 'Noticias de la 27',
             'slug' => 'noticias-de-la-27',
             'link_drive' => '#',
             'link_image' => '',
-            'description' => ''
+            'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores velit rerum sapiente vitae magnam fuga alias dolore quod odit suscipit odio, consequatur corrupti praesentium quas nesciunt reiciendis iusto. Velit, nisi?'
         ]);
     }
 }
--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -4,16 +4,17 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-6">
                 <h1 class="py-4 mx-auto text-4xl leading-8 font-bold" style="border-bottom: 4px solid #1e4a6d; text-align:center">{{$category->name}}</h1>
-                <div class="grid grid-cols-3 py-6 gap-6">
-                    <div class="md:col-span-2">
+                <div class="grid grid-cols-1 lg:grid-cols-3 py-6 gap-6">
+                    <div class="lg:col-span-2 flex flex-row w-full">
                         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg py-8 px-6">
                             <div class="text-xl mb-4 text-gray-800 text-justify" style="padding-right:10px; padding-left:10px">
-                                <h1 class="py-2 mx-auto text-2xl leading-8 font-bold" style="border-bottom: 4px solid #1e4a6d; text-align:center">DESCRIPCIÓN</h1>
-                                {!! $category->description !!}
+                                <h1 class="py-2 mx-auto text-2xl leading-8 font-bold" style="border-bottom: 4px solid #1e4a6d; text-align:center">MATERIAS</h1>
+                                <img src="{{$category->link_image}}" alt="" class="mt-6">
+                                <span class="text-sm">*Según Resolución Nº 4149/SSGECP/2012 - <a href="https://www.buenosaires.gob.ar/sites/gcaba/files/folleto-tecnica-2013_web_0_0.pdf" class="font-bold">Acceda al folleto aquí</a> </span>
                             </div>
                         </div>
                     </div>
-                    <div>
+                    <div class="flex flex-row w-full">
                         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg py-8 px-6">
                             <div class="text-xl mb-4 text-gray-800 text-justify" style="padding-right:10px; padding-left:10px">
                                 <h1 class="py-2 mx-auto text-2xl leading-8 font-bold" style="border-bottom: 4px solid #1e4a6d; text-align:center">LISTADO DRIVE</h1>
@@ -27,20 +28,28 @@
                                 </ul>
                             </div>
                         </div>
-                        <div class="md:col-start-3">
-                            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg py-8 px-6">
-                                <div class="text-xl mb-4 text-gray-800 text-justify" style="padding-right:10px; padding-left:10px">
-                                    <h1 class="py-2 mx-auto text-2xl leading-8 font-bold" style="border-bottom: 4px solid #1e4a6d; text-align:center">ULTIMAS NOTICIAS</h1>
-                                    @foreach ($posts as $post)
-                                        <div class="bg-white overflow-hidden shadow sm:rounded-lg p-4 mb-2 mt-2">
-                                            <a href="{{route('post.show',$post)}}">
-                                                <span class="text-gray-800 font-bold text-lg">{{$post->name}}<br><center class="text-gray-600 text-base">({{date('d-m-Y', strtotime($post->updated_at))}})</center></span>
-                                                <span class="text-gray-800 text-base">{{$post->extract}}</span>
-                                            </a>
-                                        </div>
-                                    @endforeach
-                                </div>
+                    </div>
+                </div>
+                <div class="grid grid-cols-1 lg:grid-cols-3 py-6 gap-6">
+                    <div class="flex flex-row lg:col-span-2 w-full">
+                        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg py-8 px-6">
+                            <div class="text-xl mb-4 text-gray-800 text-justify" style="padding-right:10px; padding-left:10px">
+                                <h1 class="py-2 mx-auto text-2xl leading-8 font-bold mb-2" style="border-bottom: 4px solid #1e4a6d; text-align:center">DESCRIPCIÓN</h1>
+                                {!! $category->description !!}
                             </div>
+                        </div>
+                    </div>
+                    <div class="flex flex-row w-full bg-white overflow-hidden shadow-xl sm:rounded-lg py-8 px-6">
+                        <div class="text-xl mb-4 text-gray-800 text-justify" style="padding-right:10px; padding-left:10px">
+                            <h1 class="py-2 mx-auto text-2xl leading-8 font-bold mb-2" style="border-bottom: 4px solid #1e4a6d; text-align:center">ULTIMAS NOTICIAS</h1>
+                            @foreach ($posts as $post)
+                                <div class="bg-white overflow-hidden shadow sm:rounded-lg p-4 mb-2 mt-2">
+                                    <a href="{{route('post.show',$post)}}">
+                                        <span class="text-gray-800 font-bold text-lg">{{$post->name}}<br><center class="text-gray-600 text-base">({{date('d-m-Y', strtotime($post->updated_at))}})</center></span>
+                                        <span class="text-gray-800 text-base">{{$post->extract}}</span>
+                                    </a>
+                                </div>
+                            @endforeach
                         </div>
                     </div>
                 </div>
