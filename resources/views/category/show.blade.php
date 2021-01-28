176eb3e831ca19c7df4a56904775700aeaa55e25
archeusWLA <64558342+archeusWLA@users.noreply.github.com>
2021-01-28 05:25:54 -0300

    Correción en show de categorias, malditas columnas

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -14,7 +14,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="flex flex-row w-full">
+                    <div class="w-full">
                         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg py-8 px-6">
                             <div class="text-xl mb-4 text-gray-800" style="padding-right:10px; padding-left:10px">
                                 <h1 class="py-2 mx-auto text-2xl leading-8 font-bold" style="border-bottom: 4px solid #1e4a6d; text-align:center">LISTADO DRIVE</h1>
