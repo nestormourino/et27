3d793cb08ba9a0a27404b861f2c96f88dfbead52
archeusWLA <64558342+archeusWLA@users.noreply.github.com>
2021-01-30 20:43:22 -0300

    Deploy 2.2

--- resources/views/components/portadaalt.blade.php
+++ resources/views/components/portadaalt.blade.php
@@ -2,7 +2,7 @@
     <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg  mt-12">
-                <img src="https://www.et27de18.com/public/images/main_alt.png" alt="">
+                <img src="storage/images/main_alt.png" alt="">
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="https://www.et27de18.com/css/app.css">
+        <link rel="stylesheet" href="/css/app.css">
         <style>
             /* RED BORDER ON INVALID INPUT */
             .check input:invalid {
@@ -34,19 +34,19 @@
         </style>
 
         <!-- Favs -->
-        <link rel="apple-touch-icon" sizes="57x57" href="https://www.et27de18.com/favicons/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="https://www.et27de18.com/favicons/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="https://www.et27de18.com/favicons/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="https://www.et27de18.com/favicons/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="https://www.et27de18.com/favicons/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="https://www.et27de18.com/favicons/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="https://www.et27de18.com/favicons/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="https://www.et27de18.com/favicons/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="https://www.et27de18.com/favicons/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192"  href="https://www.et27de18.com/favicons/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="https://www.et27de18.com/favicons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="https://www.et27de18.com/favicons/favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="https://www.et27de18.com/favicons/favicon-16x16.png">
+        <link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/favicons/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/favicons/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/favicons/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/favicons/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/favicons/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/favicons/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/favicons/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="/favicons/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
         <link rel="manifest" href="favicons/manifest.json">
         <meta name="msapplication-TileColor" content="#ffffff">
         <meta name="msapplication-TileImage" content="favicons/ms-icon-144x144.png">
@@ -79,7 +79,7 @@
     </footer>
 
     <!-- Scripts -->
-    <script src="https://www.et27de18.com/js/app.js"></script>
+    <script src="/js/app.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     <script>
         //RED BORDER ON INVALID INPUT
