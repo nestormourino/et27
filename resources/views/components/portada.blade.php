489df1872a7eaf815ab076ed82d6bcf418c87934
archeusWLA <64558342+archeusWLA@users.noreply.github.com>
2021-01-30 21:19:40 -0300

    Imagenes hinchabolas

--- resources/views/components/portada.blade.php
+++ resources/views/components/portada.blade.php
@@ -2,7 +2,7 @@
     <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <img src="../storage/images/main.png" alt="">
+                <img src="images/main.png" alt="">
             </div>
         </div>
     </div>
--- resources/views/components/portadaalt.blade.php
+++ resources/views/components/portadaalt.blade.php
@@ -2,7 +2,7 @@
     <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg  mt-12">
-                <img src="storage/images/main_alt.png" alt="">
+                <img src="images/main_alt.png" alt="">
             </div>
         </div>
     </div>
